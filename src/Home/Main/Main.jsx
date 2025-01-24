031fb237aab5376671fbe1e864d2493b1f190450
Alex Rotar <96844939+alecs007@users.noreply.github.com>
2025-01-24 16:49:00 +0200

    Modified

--- src/Home/Main/Main.jsx
+++ src/Home/Main/Main.jsx
@@ -1,7 +1,128 @@
 import "./Main.css";
+import reload from "../../assets/reload.png";
 
 const Main = () => {
-  return <div className="main">Main</div>;
+  return (
+    <div className="main">
+      <div className="timer">1:00</div>
+      <input type="text" placeholder="Start typing..."></input>
+      <div className="wordcontainer">
+        <div className="word">romania</div>
+        <div className="word">cactus</div>
+        <div className="word">eucalips</div>
+        <div className="word">unitedstatesofame</div>
+        <div className="word">china</div>
+        <div className="word">asdaaaa </div>
+        <div className="word">safasfasf</div>
+        <div className="word">rofsasfsmania</div>
+        <div className="word">romansdaia</div>
+        <div className="word">sfasfaf</div>
+        <div className="word">sfs</div>
+        <div className="word">sfasfafafa</div>
+        <div className="word">rerrgrgrg</div>
+        <div className="word">romania</div>
+        <div className="word">fgfgfdsg</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">dgsdgsdgdsg</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">dgdg</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">dggdgdg</div>
+        <div className="word">romania</div>
+        <div className="word">romagdnia</div>
+        <div className="word">gg</div>
+        <div className="word">romania</div>
+        <div className="word">gg</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">dgdsgsgsgdg</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">gsdsgdfg</div>
+        <div className="word">dgsgs</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">23123123131</div>
+        <div className="word">romania</div>
+        <div className="word">rosdasdasdmania</div>
+        <div className="word">romania</div>
+        <div className="word">sdasdadasd</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">sdsd</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romsdasdaania</div>
+        <div className="word">romania</div>
+        <div className="word">sdsd</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">ss</div>
+        <div className="word">adsdadadasd</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">sdsd</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">ss</div>
+        <div className="word">adsdadadasd</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">sdsd</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">ss</div>
+        <div className="word">adsdadadasd</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+        <div className="word">romania</div>
+      </div>
+      <div className="utilitycontainer">
+        <img src={reload} alt="Reload" />
+        <div className="stats">
+          <div className="statsitem">WPM: 56</div>
+          <div className="statsitem">Accuracy: 100%</div>
+          <div className="statsitem">Incorrect Words: 0</div>
+        </div>
+        <button className="playagain">Play Again</button>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
